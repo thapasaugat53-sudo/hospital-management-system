583e1d51e7cf5d870c96961385e3d3c09c4c36a1
thapasaugat <thapasaugat53@gmail.com>
2026-08-18 22:25:51 +0545

    user registration and role system

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
+
+def register(request):
+
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    else:
+        form = RegistrationForm()
+
+    return render(
+        request,
+        "accounts/register.html",
+        {"form": form}
+    )
