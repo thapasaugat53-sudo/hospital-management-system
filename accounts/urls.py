f6b363c3075efc41b22835bf3ee8f40151f167d9
thapasaugat <thapasaugat53@gmail.com>
2026-08-21 22:15:47 +0545

    authencation and dashboards

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,24 @@ from . import views
 
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
+
+    path(
+        "dashboard/admin/",
+        views.admin_dashboard,
+        name="admin_dashboard"
+    ),
+
+    path(
+        "dashboard/doctor/",
+        views.doctor_dashboard,
+        name="doctor_dashboard"
+    ),
+
+    path(
+        "dashboard/patient/",
+        views.patient_dashboard,
+        name="patient_dashboard"
+    ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
-
     if request.method == "POST":
         form = RegistrationForm(request.POST)
 
@@ -19,3 +20,70 @@ def register(request):
         "accounts/register.html",
         {"form": form}
     )
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+
+            if user.is_superuser:
+                return redirect("admin_dashboard")
+
+            role = user.profile.role
+
+            if role == "ADMIN":
+                return redirect("admin_dashboard")
+
+            elif role == "DOCTOR":
+                return redirect("doctor_dashboard")
+
+            elif role == "PATIENT":
+                return redirect("patient_dashboard")
+
+            return redirect("dashboard")
+
+        return render(
+            request,
+            "accounts/login.html",
+            {"error": "Invalid username or password."}
+        )
+
+    return render(request, "accounts/login.html")
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def admin_dashboard(request):
+    return render(
+        request,
+        "accounts/admin_dashboard.html"
+    )
+
+
+@login_required
+def doctor_dashboard(request):
+    return render(
+        request,
+        "accounts/doctor_dashboard.html"
+    )
+
+
+@login_required
+def patient_dashboard(request):
+    return render(
+        request,
+        "accounts/patient_dashboard.html"
+    )
+
